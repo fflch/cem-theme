7db363c50a969a26faf0e063fa331a8747a935ec
Gustavo Martusewicz <gustavp_crj@hotmail.com>
2019-03-07 18:56:08 -0300

    process file

--- assets/js/main.js
+++ assets/js/main.js
@@ -383,6 +383,36 @@
 })(window, document, jQuery);
 (function (window, document, $) {
     'use strict';
+    if ($('.que-somos-menu-col').length) {
+        
+
+        var $body = $('body');
+        var $menuMobileSideBarContainer = document.createElement('div');
+        var $menuMobileSideBarTrigger = document.createElement('div');
+        var $menuMobileSideBarTriggerInner = document.createElement('span');
+        var $menuMobileSideBarTriggerIcon = document.createElement('span');
+
+        $($menuMobileSideBarContainer).addClass('menu-side-bar-mobile');
+        $($menuMobileSideBarTrigger).addClass('menu-side-bar-mobile-trigger');
+        $($menuMobileSideBarTriggerInner).text('Menu');
+        $($menuMobileSideBarTriggerIcon).addClass('icon');
+
+        $($menuMobileSideBarTriggerInner).append($menuMobileSideBarTriggerIcon);
+        $($menuMobileSideBarTrigger).append($menuMobileSideBarTriggerInner);
+        $($menuMobileSideBarContainer).append($menuMobileSideBarTrigger);
+        $('.que-somos-menu-col').append($menuMobileSideBarContainer);
+
+        $($menuMobileSideBarTrigger).on('click',function(){
+            $($body).toggleClass('show-mobile-menu-side-bar');
+            $('.quem-somos-menu').removeClass('fade-out');
+            $('.quem-somos-menu').addClass('fade-in');
+        });
+        
+
+    }
+})(window, document, jQuery);
+(function (window, document, $) {
+    'use strict';
 
     if ($('.download-item.with-map').length) {
         var $mapItem = $('.download-item.with-map');
