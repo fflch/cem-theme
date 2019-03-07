cc1eaaefde3fc8847229d6f9aeddcb12d2e410f5
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-03-07 11:46:31 -0300

    add chars length check

--- assets/js-dev/_mapoteca.js
+++ assets/js-dev/_mapoteca.js
@@ -7,14 +7,18 @@
         $mapItem.each(function(){
             $(this).children('.texto').clone().addClass('textoHide').addClass('hide').removeClass('texto').insertBefore($(this).children('.texto'));
 
-            var $text = $(this).children('.texto').text().substring(0,250)+'...';
-            $(this).children('.texto').text($text);
-            console.log($text);
+            if( $(this).children('.texto').text().length >= 250 ){
+                var $text = $(this).children('.texto').text().substring(0,250)+'...';
+                $(this).children('.texto').text($text);
+                console.log($text);
+            }
+
 
             $(this).children('.btn-show-more').on('click',function(){
                 $(this).toggleClass('show');
                 $(this).parent('div').children('.texto').toggleClass('hide');
                 $(this).parent('div').children('.textoHide').toggleClass('hide');
+                $(this).parent('div').toggleClass('show-content');
             });
         });
     }
