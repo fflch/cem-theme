40ccc3c114d8cc927cf1b63ceffea22e6d4d8f63
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-03-07 11:45:28 -0300

    home 'pesquisadores' slider mobile config

--- assets/js-dev/_home-pesquisadores.js
+++ assets/js-dev/_home-pesquisadores.js
@@ -11,7 +11,40 @@
             speed: 300,
             slidesToShow: 5,
             slidesToScroll: 5,
-            arrows: false
+            arrows: false,
+            responsive: [
+              {
+                breakpoint: 1200,
+                settings: {
+                  slidesToShow: 4,
+                  slidesToScroll: 4
+                }
+              },
+              {
+                breakpoint: 990,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 3
+                }
+              },
+              {
+                breakpoint: 600,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2
+                }
+              },
+              {
+                breakpoint: 380,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              }
+              // You can unslick at a given breakpoint now by adding:
+              // settings: "unslick"
+              // instead of a settings object
+            ]
         });
     }
 })(window, document, jQuery);
