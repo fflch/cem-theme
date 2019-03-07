073176c77ea237859e5613cd3de2a42cf86217ee
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-03-07 11:45:50 -0300

    home '´publications' slider mobile config

--- assets/js-dev/_home-publications.js
+++ assets/js-dev/_home-publications.js
@@ -11,7 +11,48 @@
             speed: 300,
             slidesToShow: 3,
             slidesToScroll: 3,
-            arrows: false
+            arrows: false,
+            responsive: [
+              {
+                breakpoint: 990,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2,
+                  infinite: true,
+                  dots: true
+                }
+              },
+              {
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                  infinite: true,
+                  dots: true
+                }
+              },
+              {
+                breakpoint: 600,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2,
+                  infinite: true,
+                  dots: true
+                }
+              },
+              {
+                breakpoint: 450,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                  infinite: true,
+                  dots: true
+                }
+              }
+              // You can unslick at a given breakpoint now by adding:
+              // settings: "unslick"
+              // instead of a settings object
+            ]
         });
     }
 })(window, document, jQuery);
