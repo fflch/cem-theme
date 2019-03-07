89272a788172afe7713620ccdccd006e0f835874
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-03-07 11:44:32 -0300

    processed file

--- assets/js/main.js
+++ assets/js/main.js
@@ -85,6 +85,97 @@
   });
 
 })(window, document, jQuery);
+(function (window, document, $) {
+    'use strict';
+    if ($('.footer-newsletter').length) {
+ 
+            
+        // footer newsletter
+        $('.footer-newsletter .inner .container-fluid .row .col-md-7').addClass('col-sm-7 col-xs-12');
+        $('.footer-newsletter .inner .container-fluid .row .col-md-5').addClass('col-sm-5 col-xs-12');
+
+    }
+
+    if ($('.footer-navigation').length) {
+ 
+            
+        // footer newsletter
+        $('.footer-navigation .inner .container-fluid .row .col-md-8').addClass('col-sm-8 col-xs-12');
+        $('.footer-navigation .inner .container-fluid .row .col-md-4').addClass('col-sm-4 col-xs-12');
+        $('.footer-nav-infos .col-md-5').addClass('col-sm-12 col-xs-12');
+        $('.footer-nav-infos .col-md-3').addClass('col-sm-6 col-xs-6');
+
+    }
+})(window, document, jQuery);
+(function (window, document, $) {
+    'use strict';
+    if ($('.home-news-and-agenda').length) {
+        // home news
+        $('.home-news-and-agenda .home-news').parent('div').addClass('col-sm-8 col-xs-12');
+        $('.home-news-and-agenda .home-news > .row >.col-md-4').each(function(){
+            $(this).addClass('col-sm-4 col-xs-6');
+        });
+        
+        // home agenda
+        $('.home-news-and-agenda .home-agenda').parent('div').addClass('col-sm-4 col-xs-12');
+        
+        // home videos
+        $('.home-videos-and-publications .home-video').parent('div').addClass('col-sm-4 col-xs-6 home-video-wrapper');
+        
+        // home publicacoes
+        $('.home-videos-and-publications .home-publications').parent('div').addClass('col-sm-8 col-xs-6 home-publications-wrapper');
+        
+        // home apoio
+        
+        // $('.home-apoio .logos > .row >.col-md-3').each(function(){
+            //     $(this).addClass('col-sm-6 col-xs-6');
+            // });
+            
+
+    }
+})(window, document, jQuery);
+(function(window, document, $) {
+    'use strict';
+    if ($('.home-apoio').length) {
+        $('.home-apoio div.logos>div.row').slick({
+            autoplay: true,
+            autoplaySpeed: 6000,
+            dots: false,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            arrows: false,
+            dots: true,
+            responsive: [
+              {
+                breakpoint: 1000,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 3
+                }
+              },
+              {
+                breakpoint: 680,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2
+                }
+              },
+              {
+                breakpoint: 480,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              }
+              // You can unslick at a given breakpoint now by adding:
+              // settings: "unslick"
+              // instead of a settings object
+            ]
+        });
+    }
+})(window, document, jQuery);
 (function(window, document, $) {
     'use strict';
     if ($('.home-pesquisadores').length) {
@@ -98,7 +189,40 @@
             speed: 300,
             slidesToShow: 5,
             slidesToScroll: 5,
-            arrows: false
+            arrows: false,
+            responsive: [
+              {
+                breakpoint: 1200,
+                settings: {
+                  slidesToShow: 4,
+                  slidesToScroll: 4
+                }
+              },
+              {
+                breakpoint: 990,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 3
+                }
+              },
+              {
+                breakpoint: 600,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2
+                }
+              },
+              {
+                breakpoint: 380,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              }
+              // You can unslick at a given breakpoint now by adding:
+              // settings: "unslick"
+              // instead of a settings object
+            ]
         });
     }
 })(window, document, jQuery);
@@ -115,7 +239,48 @@
             speed: 300,
             slidesToShow: 3,
             slidesToScroll: 3,
-            arrows: false
+            arrows: false,
+            responsive: [
+              {
+                breakpoint: 990,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2,
+                  infinite: true,
+                  dots: true
+                }
+              },
+              {
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                  infinite: true,
+                  dots: true
+                }
+              },
+              {
+                breakpoint: 600,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2,
+                  infinite: true,
+                  dots: true
+                }
+              },
+              {
+                breakpoint: 450,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                  infinite: true,
+                  dots: true
+                }
+              }
+              // You can unslick at a given breakpoint now by adding:
+              // settings: "unslick"
+              // instead of a settings object
+            ]
         });
     }
 })(window, document, jQuery);
@@ -130,7 +295,30 @@
             speed: 300,
             slidesToShow: 3,
             slidesToScroll: 3,
-            arrows: false
+            arrows: false,
+            responsive: [
+              {
+                breakpoint: 990,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2,
+                  infinite: true,
+                  dots: true
+                }
+              },
+              {
+                breakpoint: 480,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                  infinite: true,
+                  dots: true
+                }
+              }
+              // You can unslick at a given breakpoint now by adding:
+              // settings: "unslick"
+              // instead of a settings object
+            ]
         });
     }
 })(window, document, jQuery);
@@ -138,7 +326,8 @@
     'use strict';
     if ($('.home-video').length) {
         // $('.home-video div.home-video-box div.inner').slick({
-        $('.home-video div.home-video-box div.inner>div.row>div.views-element-container>div').slick({
+        // $('.home-video div.home-video-box div.inner>div.row>div.views-element-container>div').slick({
+        $('.home-video div.home-video-box div.inner>div.views-element-container>div').slick({
             autoplay: true,
             autoplaySpeed: 6000,
             dots: true,
@@ -220,6 +409,74 @@
             
         }
     }
+})(window, document, jQuery);
+(function(window, document, $) {
+    'use strict';
+    var $body = $('body');
+    var $headerMenu = $('.header-menu');
+    var $mobileHeaderMenuContainer = document.createElement('div');
+    var $mobileHeaderMenuTrigger = document.createElement('div');
+    var $mobileHeaderMenuTriggerInnerEl = document.createElement('span');
+    var $mobileHeaderMenuClose = document.createElement('div');
+    var $mobileHeaderMenuCloseInnerEl = document.createElement('span');
+
+    var $headerMenuItems = $('.header-menu .inner>ul>li');
+
+    $($mobileHeaderMenuTrigger).addClass('header-menu-mobile-trigger');
+    $($mobileHeaderMenuContainer).addClass('header-menu-mobile');
+
+    $($mobileHeaderMenuTrigger).append($mobileHeaderMenuTriggerInnerEl);
+    $($mobileHeaderMenuContainer).append($mobileHeaderMenuTrigger);
+
+    $('header.header').append($mobileHeaderMenuContainer);
+
+    $($mobileHeaderMenuClose).append($mobileHeaderMenuCloseInnerEl);
+    $($mobileHeaderMenuClose).addClass('mobile-close')
+    $('.header-menu').append($mobileHeaderMenuClose);
+
+
+    $($mobileHeaderMenuTrigger).on('click', function() {
+        $($body).toggleClass('show-mobile-menu');
+        $headerMenu.removeClass('fade-out');
+        $headerMenu.addClass('fade-in');
+
+    })
+    $($mobileHeaderMenuClose).on('click', function() {
+        clearShowDropDown();
+        $headerMenu.removeClass('fade-in');
+        $headerMenu.addClass('fade-out');
+        setTimeout(function(){
+            $($body).toggleClass('show-mobile-menu');
+        }, 200)
+    })
+
+    function clearShowDropDown() {
+        $($headerMenuItems).each(function() {
+            $(this).removeClass('show-drop-down-mobile')
+        });
+    };
+
+
+    // get menu items
+    $($headerMenuItems).each(function(index) {
+        console.log(this, index);
+        var $menuPlus = document.createElement('div');
+        var $menuPlusInner = document.createElement('span');
+        $($menuPlus).addClass('menu-mobile-open-drop');
+        $($menuPlus).append($menuPlusInner);
+        $(this).children('a').append($menuPlus);
+        $($menuPlus).on('click', function(e) {
+            e.preventDefault();
+            if ($(this).parent('a').parent('li').hasClass('show-drop-down-mobile')) {
+                $(this).parent('a').parent('li').removeClass('show-drop-down-mobile');
+                return;
+            }
+            clearShowDropDown();
+            $(this).parent('a').parent('li').toggleClass('show-drop-down-mobile');
+        });
+        // console.log( $(this) );
+    });
+
 })(window, document, jQuery);
 (function(window, document, $) {
   "use strict";
